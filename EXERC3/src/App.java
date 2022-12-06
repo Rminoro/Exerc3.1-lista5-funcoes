38f659f78b97f930ffebd363c3cdf2fcf255aa77
Rminoro <rminoro@outlook.com>
2022-12-05 22:18:28 -0300

    Exerc3.1-lista5-funcoes
    Exercício 3 da lista de funções

---
+++ EXERC3/src/App.java
@@ -0,0 +1,24 @@
+import java.util.function.Function;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner S = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro numero");
+        double numero1 = S.nextDouble();
+
+        System.out.println("Informe o segundo numero");
+        double numero2 = S.nextDouble();
+
+        System.out.println("Informe o terceiro numero");
+        double numero3 = S.nextDouble();
+
+        soma(numero1, numero2, numero3);
+        soma(10,20,30);
+    }
+
+    static void soma(double a, double b, double c) {
+        System.out.println(a + b + c);
+    }
+}
